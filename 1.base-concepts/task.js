eb2421aae9d5d6e2ef1356cfd43ac0ff7520bca1
MarkDolgin <33152312+MarkDolgin@users.noreply.github.com>
2023-08-14 14:36:19 +0300

    task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,45 @@
-"use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+
+  let discriminant = b ** 2 - 4 * a * c;
+
+  if (discriminant < 0) {
+    return arr;
+  } else if (discriminant === 0) {
+    let x = -b / (2 * a);
+    arr.push(x);
+  } else {
+    let x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    let x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    arr.push(x1, x2);
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if (percent > 0) {
+    percent = +percent;
+  } else if (percent != 0 || percent == 0) {
+    return `"Параметр "Процентная ставка" содержит неправильное значение ${percent}".`;
+  }
+
+  if (contribution >= 0) {
+    contribution = +contribution;
+  } else if (contribution != 0) {
+    return `"Параметр "Начальный взнос" содержит неправильное значение ${contribution}".`;
+  }
+
+  if (amount >= 0) {
+    amount = +amount;
+  } else if (amount != 0) {
+    return `"Параметр "Общая стоимость" содержит неправильное значение ${amount}".`;
+  }
+  let returnAmount = amount - contribution;
+  percent = percent / 1200;
+  let monthlyPay =
+    amount * (percent + percent / ((1 + percent) ** countMonths - 1));
+  let totalAmount = monthlyPay * countMonths;
+  console.log(totalAmount.toFixed(2));
+  return totalAmount.toFixed(2);
 }
