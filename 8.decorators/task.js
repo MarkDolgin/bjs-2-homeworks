b0a87eae47f46b0d4a2b6089e3d115a8d5d44bfe
MarkDolgin <33152312+MarkDolgin@users.noreply.github.com>
2023-08-30 13:23:15 +0300

    task.js

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,44 @@
-//Задача № 1
 function cachingDecoratorNew(func) {
-  
-}
+  let cache = [];
+  const maxCacheValuesCount = 5;
+  return (...args) => {
+    const hash = md5(args);
+    const camelCase = cache.find((object) => object.hash === hash);
+    if (camelCase) {
+      console.log("Из кеша: " + camelCase.value);
+      return "Из кеша: " + camelCase.value;
+    }
+
+    let result = func(...args);
+    cache.push({ hash: hash, value:result });
+    if (cache.length > maxCacheValuesCount) {
+      cache.shift();
+    }
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;  
+  }
+} 
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
-}
+  let timeoutId = null;
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+
+  function wrapper(...args) {
+    if (timeoutId) {
+        clearTimeout(timeoutId);
+    }
+      if (!timeoutId) {
+          func(...args);
+          wrapper.count++;
+          }
+      
+          timeoutId = setTimeout(() => {
+              func(...args)
+              wrapper.count++;
+      }, delay);
+      wrapper.allCount++;
+  }
+  return wrapper;
+
